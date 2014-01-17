2c884d1244dbce6b8813eb25e3f1c2f86e5afef7
felipejimenez <lipe2301@hotmail.com>
2014-01-16 22:47:05 -0300

    acrescentando algumas coisas

--- src/br/edu/ifms/pykota/main/Main.java
+++ src/br/edu/ifms/pykota/main/Main.java
@@ -1,14 +1,18 @@
 package br.edu.ifms.pykota.main;
 
-public class Main {
+import br.edu.ifms.pykota.entidade.*;
+import javax.persistence.Query;
+import org.hibernate.Session;
+import br.edu.ifms.pykota.entidade.Users;
 
+public class Main {
+	private static Session session;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("Mah oi lomardi");
+		
 		
 	}
 
