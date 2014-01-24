fbd4c03e5f6159d7ff26a8493ec935b27fa4422f
felipejimenez <lipe2301@hotmail.com>
2014-01-24 14:17:44 -0300

    mapeamento com netbeans

--- src/br/edu/ifms/pykota/controle/Cadastros.java
+++ src/br/edu/ifms/pykota/controle/Cadastros.java
@@ -2,13 +2,17 @@ package br.edu.ifms.pykota.controle;
 
 import java.util.Random;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.swing.JOptionPane;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.Transaction;
 
-import org.hibernate.Session;
+import org.hibernate.*;
 
+
+import br.edu.ifms.pykota.dao.DAOHibernateUtil;
 import br.edu.ifms.pykota.entidades.Users;;
 
 public class Cadastros {
@@ -16,35 +20,23 @@ public class Cadastros {
 /*essa classe insere novos usuarios a partir do pykotaadmin, setado no sql*/
 public void Insere_User() {		
 		
-	
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("pykota"); 
-		
-		/*manipular as entidades da nossa aplicação*/
-		EntityManager em = emf.createEntityManager();
-		
-		Session session = (Session) em.getDelegate();
+		Session session = DAOHibernateUtil.getSessionFactory().getCurrentSession();
 		
 		Users dados = new Users();
 		
-		em.getTransaction().begin(); 
-		
 		Random gerador = new Random();
-		dados.setUsername("teste"+gerador.nextInt());
+		dados.setUsername("Felipe"+gerador.nextInt());
 		dados.setEmail("Tads");
 		dados.setLifetimepaid((float) 1.0);
 		dados.setDescription("teste");
 		dados.setLimitby("teste");
 		dados.setBalance((float) 1.0);
 		
-		/*armazena as informações de um objeto no banco de dados, */
-		em.persist(dados);
-		
-		
-		/*Para conﬁrmar uma transação, devemos usar o método commit(). Quando esse método é invo-
-		do, ocorre uma sincronização como banco de dados e a transação é ﬁnalizada.*/
-		em.getTransaction().commit(); 
+		session.beginTransaction();
+		session.save(dados);
+		session.getTransaction().commit();
 		JOptionPane.showMessageDialog(null, "Gravado com sucesso !");
-		emf.close();
+		
 	}
 	
 }
--- src/br/edu/ifms/pykota/main/Main.java
+++ src/br/edu/ifms/pykota/main/Main.java
@@ -1,13 +1,29 @@
 package br.edu.ifms.pykota.main;
 
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
 import br.edu.ifms.pykota.controle.Cadastros;
 
-public class Main {
+public class Main extends JFrame{
 
+	public Main(){
+		
+		
+		
+		this.setLayout(null);
+	    this.setExtendedState(MAXIMIZED_BOTH);
+	    this.setVisible(true);
+	    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		Cadastros cadastro = new Cadastros();
 		cadastro.Insere_User();
-
+		System.exit(0);
 	}
 
 }
